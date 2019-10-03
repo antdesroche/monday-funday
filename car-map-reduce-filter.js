fc3bdfcabbd507d23315e5c32585fca1c6361265
Antonio Desroche <antdesroche@gmail.com>
2019-10-02 21:21:53 -0400

    Making a map of the objects with an added display

--- car-map-reduce-filter.js
+++ car-map-reduce-filter.js
@@ -4,10 +4,15 @@ var cars = [
     { make: 'Nissan', model: 'Altima', year:2005 }
 ]
 
-
+let displayData = cars.map((car) => {
+    let result = car
+result.display = car.year + '' + car.make + '' + car.model
+return result
+}
+)
 let carYears = cars.filter( (car) => { 
     return car.year < 2005
 }
 )
 
-console.log(carYears)
+console.log(displayData)
